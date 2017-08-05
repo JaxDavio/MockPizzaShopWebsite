0775cea41ab2fe9c1839b53ad8d22ae4be4f3968
David <jaxdavio@yahoo.com>
2017-08-05 00:20:24 -0700

    NewUpload

---
+++ js/pizza_alert.js
@@ -0,0 +1,7 @@
+		
+		var VisitorName = prompt("Enter your First & Last Name for a chance to win a Free Pizza!");
+		
+		/*var result = VisitorName.fontcolor("white");*/
+		
+		document.write("<b><i>CONGRATULATIONS, " + VisitorName + "! </i><br /> You've just been entered into our <br /> monthly drawing for a Free Pizza!</b>");
+		
